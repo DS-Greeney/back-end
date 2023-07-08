7700e25a1b02a1d837bcc548097264ccfe61ad67
mk020 <mk02081@naver.com>
2023-07-08 23:30:38 +0900

    login

--- src/main/java/com/greeneyback/member/dto/MemberDTO.java
+++ src/main/java/com/greeneyback/member/dto/MemberDTO.java
@@ -1,5 +1,6 @@
 package com.greeneyback.member.dto;
 
+import com.greeneyback.member.entity.MemberEntity;
 import lombok.*;
 
 @Getter
@@ -12,4 +13,13 @@ public class MemberDTO {
     private String userNickname;
     private String userEmail;
     private String userPassword;
+
+    public static MemberDTO toMemberDTO(MemberEntity memberEntity) {
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setUserId(memberEntity.getUserId());
+        memberDTO.setUserNickname(memberEntity.getUserNickname());
+        memberDTO.setUserEmail(memberEntity.getUserEmail());
+        memberDTO.setUserPassword(memberEntity.getUserPassword());
+        return memberDTO;
+    }
 }

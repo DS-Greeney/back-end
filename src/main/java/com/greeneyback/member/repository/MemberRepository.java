22c90472b61d2b1a91021c10d2e93e52fd93ea8c
mk020 <mk02081@naver.com>
2023-07-08 23:30:38 +0900

    login

--- src/main/java/com/greeneyback/member/repository/MemberRepository.java
+++ src/main/java/com/greeneyback/member/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.greeneyback.member.repository;
 import com.greeneyback.member.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    // 이메일로 회원 정보 조회 (select * from user_table where user_email=?)
+    Optional<MemberEntity> findByUserEmail(String userEmail);
 }

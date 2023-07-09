469cf4a0e65a918db26870edcfe78c489817e4e4
KangSumin <eeeebul@naver.com>
2023-07-09 19:27:04 +0900

    회원탈퇴 & 로그아웃

--- src/main/java/com/greeneyback/member/controller/MemberController.java
+++ src/main/java/com/greeneyback/member/controller/MemberController.java
@@ -4,10 +4,11 @@ import com.greeneyback.member.dto.MemberDTO;
 import com.greeneyback.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,6 +27,60 @@ public class MemberController {
         System.out.println("MemberController.save");
         System.out.println("memberDTO = " + memberDTO);
         memberService.save(memberDTO);
+        return "login";
+    }
+
+    @GetMapping("/member/login")
+    public String loginForm() {
+        return "login";
+    }
+
+    @PostMapping("/member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) { // session - 로그인정보를 유지
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if (loginResult != null) {
+            // login 성공 - main페이지로
+            session.setAttribute("loginId", loginResult.getUserId());
+            session.setAttribute("loginEmail", loginResult.getUserEmail()); // 로그인한 유저의 이메일 정보를 세션에 담아준다, loginEmail로 사용
+            session.setAttribute("loginNickname", loginResult.getUserNickname());
+            return "main";
+        } else {
+            // 로그인 실패(return값이 null) - 다시 로그인 페이지로
+            return "login";
+        }
+    }
+    @GetMapping("/member/")
+    public String findAll(Model model) {
+        List<MemberDTO> memberDTOList = memberService.findAll();
+        // 어떠한 html로 가져갈 데이터가 있다면 model사용
+        model.addAttribute("memberList", memberDTOList);
+        return "list";
+    }
+    @GetMapping("/member/update")
+    public String updateForm(HttpSession session, Model model) {
+        String myEmail = (String) session.getAttribute("loginEmail");
+        MemberDTO memberDTO = memberService.updateForm(myEmail);
+        model.addAttribute("updateUser", memberDTO);
+        return "update";
+    }
+
+    @PostMapping("member/update") // 변경요청
+    public String update(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+        memberService.update(memberDTO);
+        MemberDTO loginResult = memberService.login(memberDTO);
+        session.setAttribute("loginNickname", loginResult.getUserNickname());
+        return "main";
+    }
+
+    @GetMapping("/member/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
         return "index";
+    }
+    @GetMapping("/member/delete")
+    public String deleteById(HttpSession session, Model model) {
+        Long loginId = (Long) session.getAttribute("loginId");
+        memberService.deleteById(loginId);
+        return "delete";
     }
 }

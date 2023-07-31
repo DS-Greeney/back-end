ff5e3cb434d37521f7b6531374704ff7a00c599f
mk020 <mk02081@naver.com>
2023-07-31 22:19:57 +0900

    login api 구현중

--- src/main/java/com/greeneyback/member/controller/MemberAPIController.java
+++ src/main/java/com/greeneyback/member/controller/MemberAPIController.java
@@ -4,27 +4,26 @@ import com.greeneyback.member.dto.MemberDTO;
 import com.greeneyback.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 
-@CrossOrigin("http://localhost:8081")
-@Controller
+@RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 @Slf4j
 public class MemberAPIController {
 
+    @Autowired
     private final MemberService memberService;
 
     @PostMapping("/register")
-    public HashMap<String, Object> register(@RequestParam String userNickname, @RequestParam String userEmail,
-                                            @RequestParam String userPassword, @RequestParam String userPhonenum,
-                                            @RequestParam String userBirthdate, @RequestParam Integer userGender) {
+    public HashMap<String, Object> register(String userNickname, String userEmail,
+                                            String userPassword, String userPhonenum,
+                                            String userBirthdate, Integer userGender) {
 
         HashMap<String, Object> userMap = new HashMap<>();
 
@@ -50,6 +49,34 @@ public class MemberAPIController {
 
             log.info("회원가입 실패");
             return userMap;
+        }
+    }
+
+    // 수정필요~~~~~~~~~~~~~~~~
+    @PostMapping("/login")
+    public HashMap<String, Object> login(String userEmail, String userPassword) {
+
+        HashMap<String, Object> map = new HashMap<>();
+
+        try {
+            MemberDTO memberDTO = new MemberDTO();
+            memberDTO.setUserEmail(userEmail);
+            memberDTO.setUserPassword(userPassword);
+
+            MemberDTO loginResult = memberService.login(memberDTO);
+
+            map.put("success", Boolean.TRUE);
+            map.put("userId", loginResult.getUserId());
+
+            log.info("로그인 성공");
+
+            return map;
+        } catch (Exception e) {
+            map.put("success", Boolean.FALSE);
+            map.put("message", e.getMessage());
+
+            log.info("로그인 실패");
+            return map;
         }
     }
 

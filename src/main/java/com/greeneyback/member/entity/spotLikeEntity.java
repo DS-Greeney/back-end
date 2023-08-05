aaf1446247833ee624ce637781601948e527cc9e
KangSumin <eeeebul@naver.com>
2023-08-05 17:47:47 +0900

    spotLikeEntity 수정

--- src/main/java/com/greeneyback/member/entity/spotLikeEntity.java
+++ src/main/java/com/greeneyback/member/entity/spotLikeEntity.java
@@ -10,16 +10,28 @@ import javax.validation.constraints.NotNull;
 @Entity
 @Setter
 @Getter
-@Table(name = "tourspot_like_table")
-public class TourspotLikeEntity {
+@Table(name = "spot_like_table")
+public class spotLikeEntity {
     @Id // pk 지정
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int tourspotLikeId;
+    private int spotLikeId;
+
+    // 관광지 : 1 , 식당 : 2 , 숙소 : 3, 여행코스 : 4
+    @Column
+    @NotNull
+    private int categoryNumber; // 좋아요 누른 카테고리 코드
 
     @ManyToOne(cascade = CascadeType.REMOVE)
-    @NotNull
     @JoinColumn(referencedColumnName = "tourspot_id")
     TourspotEntity tourspot;
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(referencedColumnName = "rstrnt_id")
+    RstrntEntity rstrnt;
+
+    // 숙소 id  - 추후 수정
+
+    // 여행코스 id  - 추후 수정
 
     @ManyToOne(cascade = CascadeType.REMOVE)
     @NotNull

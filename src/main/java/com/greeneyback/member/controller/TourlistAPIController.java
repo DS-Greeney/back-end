04f718e3585f095fa92e00266e8238464207d986
mk020 <mk02081@naver.com>
2023-08-20 15:32:25 +0900

    식당 api 작성, 거리순 정렬

--- src/main/java/com/greeneyback/member/controller/TourlistAPIController.java
+++ src/main/java/com/greeneyback/member/controller/TourlistAPIController.java
@@ -1,6 +1,7 @@
 package com.greeneyback.member.controller;
 
 import com.greeneyback.member.entity.TourspotEntity;
+import com.greeneyback.member.service.RstrntService;
 import com.greeneyback.member.service.TourService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,10 +20,13 @@ public class TourlistAPIController {
 
     @Autowired
     private final TourService tourService;
+    @Autowired
+    private final RstrntService rstrntService;
 
     // tourList 반환 메서드
     @GetMapping("/tourlist")
-    public Object tourlist(@RequestParam(name = "latitude", defaultValue = "0") String latitude, @RequestParam(name = "longitude", defaultValue = "0") String longitude) {
+    public Object tourlist(@RequestParam(name = "latitude", defaultValue = "37.5538") String latitude, @RequestParam(name = "longitude", defaultValue = "126.9916") String longitude) {
+    //디폴트위도경도값:서울중심(남산)
 
         HashMap<String, Object> tourMap = new HashMap<>();
         HashMap<String, Double> myLocation = new HashMap<>();
@@ -37,6 +41,23 @@ public class TourlistAPIController {
             myLocation.put("longitude", Double.parseDouble(longitude));
             tourMap.put("success", Boolean.TRUE);
             tourMap.put("tourlists", tourService.findByMyLocation(myLocation));
+        } catch (Exception e) {
+            tourMap.put("error", e.getMessage());
+        }
+
+        return tourMap;
+    }
+
+    @GetMapping("restaurant")
+    public Object restaurant(@RequestParam(name = "latitude", defaultValue = "37.5538") String latitude, @RequestParam(name = "longitude", defaultValue = "126.9916") String longitude) {
+        HashMap<String, Object> tourMap = new HashMap<>();
+        HashMap<String, Double> myLocation = new HashMap<>();
+
+        try {
+            myLocation.put("latitude", Double.parseDouble(latitude));
+            myLocation.put("longitude", Double.parseDouble(longitude));
+            tourMap.put("success", Boolean.TRUE);
+            tourMap.put("restaurants", rstrntService.findByMyLocation(myLocation));
         } catch (Exception e) {
             tourMap.put("error", e.getMessage());
         }

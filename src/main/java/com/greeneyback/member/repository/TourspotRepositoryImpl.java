65cbeeac6f5ee46021f813088c3166c4580bd6b1
mk020 <mk02081@naver.com>
2023-08-20 15:32:25 +0900

    식당 api 작성, 거리순 정렬

--- src/main/java/com/greeneyback/member/repository/TourspotRepositoryImpl.java
+++ src/main/java/com/greeneyback/member/repository/TourspotRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.greeneyback.member.repository;
 
+import com.greeneyback.member.entity.RstrntEntity;
 import com.greeneyback.member.entity.TourspotEntity;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.HashMap;
 import java.util.List;
 
+import static com.greeneyback.member.entity.QRstrntEntity.rstrntEntity;
 import static com.greeneyback.member.entity.QTourspotEntity.tourspotEntity;
 
 @Repository
@@ -34,5 +36,22 @@ public class TourspotRepositoryImpl implements TourspotRepositoryCustom {
                 ).asc())
                 .fetch();
         return tourspotEntityList;
+    }
+
+    public List<RstrntEntity> findRstrntByLocation(HashMap<String, Double> myLocation) {
+        List<RstrntEntity> rstrntEntityList = queryFactory
+                .selectFrom(rstrntEntity)
+                .orderBy(Expressions.stringTemplate("ST_Distance_Sphere({0}, {1})",
+                        Expressions.stringTemplate("POINT({0}, {1})",
+                                myLocation.get("longitude"),
+                                myLocation.get("latitude")
+                        ),
+                        Expressions.stringTemplate("POINT({0}, {1})",
+                                rstrntEntity.rstrntLo,
+                                rstrntEntity.rstrntLa
+                        )
+                ).asc())
+                .fetch();
+        return rstrntEntityList;
     }
 }
--- src/main/java/com/greeneyback/member/service/RstrntService.java
+++ src/main/java/com/greeneyback/member/service/RstrntService.java
@@ -2,14 +2,17 @@ package com.greeneyback.member.service;
 
 import com.greeneyback.member.dto.RstrntDTO;
 import com.greeneyback.member.entity.RstrntEntity;
+import com.greeneyback.member.entity.TourspotEntity;
 import com.greeneyback.member.helper.CSVHelper;
 import com.greeneyback.member.repository.RstrntRepository;
+import com.greeneyback.member.repository.TourspotRepositoryImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -37,10 +40,20 @@ public class RstrntService {
 
     private final RstrntRepository rstrntRepository;
 
+    @Autowired
+    private final TourspotRepositoryImpl tourspotRepositoryImpl;
+
+    public List<RstrntEntity> findAllRstrntEntities() {
+        return rstrntRepository.findAll();
+    }
+
     public void restaurantSave(RstrntDTO rstrntDTO) {
         RstrntEntity rstrntEntity = RstrntEntity.toRstrntEntity(rstrntDTO);
         rstrntRepository.save(rstrntEntity);
     }
 
+    public List<RstrntEntity> findByMyLocation(HashMap<String, Double> myLocation) {
+        return tourspotRepositoryImpl.findRstrntByLocation(myLocation);
+    }
 
 }

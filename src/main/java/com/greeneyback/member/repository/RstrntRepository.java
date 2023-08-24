678c1f7546618573baae6c2ea2c85532ff1aa7af
mk020 <mk02081@naver.com>
2023-08-24 23:56:34 +0900

    식당 상세정보 API 작성

--- src/main/java/com/greeneyback/member/repository/RstrntRepository.java
+++ src/main/java/com/greeneyback/member/repository/RstrntRepository.java
@@ -3,5 +3,5 @@ package com.greeneyback.member.repository;
 import com.greeneyback.member.entity.RstrntEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RstrntRepository extends JpaRepository<RstrntEntity, Integer> {
+public interface RstrntRepository extends JpaRepository<RstrntEntity, String> {
 }

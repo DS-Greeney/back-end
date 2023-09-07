f3f96954d75313aec933f8f571e844704c1e2e00
mk020 <mk02081@naver.com>
2023-09-07 11:17:50 +0900

    도전과제 테이블 생성, 데이터 삽입

---
+++ src/main/java/com/greeneyback/member/entity/ChallengeEntity.java
@@ -0,0 +1,30 @@
+package com.greeneyback.member.entity;
+
+import com.greeneyback.member.dto.ChallengeDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "challenge_table")
+public class ChallengeEntity {
+    @Id
+    @Column(unique = true)
+    @NotNull
+    private int challengeId;
+
+    @Column(length = 200)
+    @NotNull
+    private String challengeContent;
+
+    public static ChallengeEntity toChallengeEntity(ChallengeDTO challengeDTO) {
+        ChallengeEntity challengeEntity = new ChallengeEntity();
+        challengeEntity.setChallengeId(challengeDTO.getChallengeId());
+        challengeEntity.setChallengeContent(challengeDTO.getChallengeContent());
+        return challengeEntity;
+    }
+}

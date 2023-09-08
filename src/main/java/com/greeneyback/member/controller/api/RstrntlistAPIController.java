a8a9ad88f9ad6326b66ba60612737cc22ebe4437
KangSumin <eeeebul@naver.com>
2023-09-09 02:44:30 +0900

    [add] - 식당 review 추가 완료

--- src/main/java/com/greeneyback/member/controller/api/RstrntlistAPIController.java
+++ src/main/java/com/greeneyback/member/controller/api/RstrntlistAPIController.java
@@ -1,13 +1,19 @@
 package com.greeneyback.member.controller.api;
 
+import com.greeneyback.member.dto.CommentDTO;
 import com.greeneyback.member.entity.RstrntEntity;
+import com.greeneyback.member.entity.SpotCommentEntity;
+import com.greeneyback.member.service.AWSS3Service;
 import com.greeneyback.member.service.RstrntService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,6 +24,7 @@ public class RstrntlistAPIController {
 
     @Autowired
     private final RstrntService rstrntService;
+    private final AWSS3Service awss3Service;
 
     @GetMapping("/restaurantlist")
     public Object restaurant(@RequestParam(name = "latitude", defaultValue = "37.5538") String latitude, @RequestParam(name = "longitude", defaultValue = "126.9916") String longitude, @RequestParam(name = "areaCode") int areaCode) {
@@ -48,14 +55,51 @@ public class RstrntlistAPIController {
     }
 
     @GetMapping("/restaurantlist/detail/{rstrntId}")
-    public HashMap<String, Object> getRestaurantlistDetail(@PathVariable String rstrntId) {
+    public HashMap<String, Object> getRestaurantlistDetail(@PathVariable int rstrntId) {
         HashMap<String, Object> map = new HashMap<>();
+        // review들을 모은 List
+        List<Object> reviewList = new ArrayList<>();
 
         try {
             Optional<RstrntEntity> restaurant = rstrntService.findRstrntDetail(rstrntId);
-            map.put("success", Boolean.TRUE);
+
+            // 리뷰 불러오기
+            reviewList = rstrntService.getReviewList(rstrntId, 2);
+            map.put("reviewList", reviewList);
             map.put("restaurant", restaurant);
+            map.put("success", Boolean.TRUE);
         } catch(Exception e) {
+            map.put("success", Boolean.FALSE);
+            map.put("error", e.getMessage());
+        }
+
+        return map;
+    }
+
+    // 리뷰 작성 post
+    @PostMapping("/tourlist/detail/{tourspotId}")
+    public HashMap<String, Object> postTourComment(@RequestParam("images") List<MultipartFile> multipartFiles, @ModelAttribute CommentDTO commentDTO) {
+        HashMap<String, Object> map = new HashMap<>();
+
+        // S3 service
+
+        try {
+            // S3에 넣고 결과 url을 담을 리스트
+            List<String> imageUrlList = new ArrayList<>();
+
+            // S3Service에게 파일값을 넘겨주고 결과 url 리스트를 받아온다
+            imageUrlList = awss3Service.uploadFiletToS3(multipartFiles);
+
+            // 리뷰 테이블에 먼저 추가한 후 그 Entity를 받아온다. (commentID 때문에)
+            SpotCommentEntity spotCommentEntity = rstrntService.saveRstrntReviewComment(commentDTO);
+
+            // commentID와 함께 이미지 db를 추가한다.
+            rstrntService.saveRstrntReviewImage(spotCommentEntity, imageUrlList);
+            map.put("success", Boolean.TRUE);
+
+        }
+        catch(Exception e) {
+            e.printStackTrace();
             map.put("success", Boolean.FALSE);
             map.put("error", e.getMessage());
         }
--- src/main/java/com/greeneyback/member/service/RstrntService.java
+++ src/main/java/com/greeneyback/member/service/RstrntService.java
@@ -1,13 +1,22 @@
 package com.greeneyback.member.service;
 
+import com.greeneyback.member.dto.CommentDTO;
 import com.greeneyback.member.dto.RstrntDTO;
+import com.greeneyback.member.entity.MemberEntity;
 import com.greeneyback.member.entity.RstrntEntity;
+import com.greeneyback.member.entity.SpotCommentEntity;
+import com.greeneyback.member.entity.SpotCommentImageEntity;
+import com.greeneyback.member.repository.MemberRepository;
 import com.greeneyback.member.repository.RstrntRepository;
+import com.greeneyback.member.repository.SpotCmntImgRepository;
+import com.greeneyback.member.repository.SpotCmntRepository;
 import com.greeneyback.member.repository.impl.RstrntRepositoryImpl;
+import com.greeneyback.member.repository.impl.SpotCmntRepositoryImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
@@ -40,15 +49,20 @@ public class RstrntService {
     @Autowired
     private final RstrntRepositoryImpl rstrntRepositoryImpl;
 
+    private final MemberRepository memberRepository;
+    private final SpotCmntRepository spotCmntRepository;
+    private final SpotCmntImgRepository spotCmntImgRepository;
+    private final SpotCmntRepositoryImpl spotCmntRepositoryImpl;
+
     public List<RstrntEntity> findAllRstrntEntities() {
         return rstrntRepository.findAll();
     }
 
-    public Optional<RstrntEntity> findById(String id) {
+    public Optional<RstrntEntity> findById(int id) {
         return rstrntRepository.findById(id);
     }
 
-    public Optional<RstrntEntity> findRstrntDetail(String rstrntId) {
+    public Optional<RstrntEntity> findRstrntDetail(int rstrntId) {
         Optional<RstrntEntity> restaurant = rstrntRepository.findById(rstrntId);
         return restaurant;
     }
@@ -65,5 +79,86 @@ public class RstrntService {
     public List<RstrntEntity> findByMyLocationAreaFilter(HashMap<String, Double> myLocation, int areaCode) {
         return rstrntRepositoryImpl.findByLocationAreaCode(myLocation, areaCode);
     }
+
+
+    // 리뷰를 db에 저장하는 메소드, 저장한 Entity를 반환한다.
+    public SpotCommentEntity saveRstrntReviewComment(CommentDTO commentDTO) {
+        SpotCommentEntity rstrntCommentEntity = new SpotCommentEntity();
+
+        // userId 찾기
+        MemberEntity memberEntity = memberRepository.findByUserId(commentDTO.getUserId());
+
+        // entity 설정
+        rstrntCommentEntity.setSpotId(commentDTO.getSpotId());
+        rstrntCommentEntity.setUser(memberEntity);
+        rstrntCommentEntity.setCategoryNumber(commentDTO.getCategoryNumber());
+        rstrntCommentEntity.setSpotCmntContent(commentDTO.getCmntContent());
+        rstrntCommentEntity.setSpotCmntStar(commentDTO.getCmntStar());
+
+        spotCmntRepository.save(rstrntCommentEntity);
+
+        return rstrntCommentEntity;
+    }
+
+    // 이미지 url을 저장하는 메소드
+    public void saveRstrntReviewImage(SpotCommentEntity rstrntCommentEntity, List<String> imgUrlList) {
+
+        // forEach문을 통해서 db에 이미지 entity 추가
+        for(String imgUrl : imgUrlList) {
+            // 이미지 entity 선언
+            SpotCommentImageEntity rstrntImageEntity = new SpotCommentImageEntity();
+            // entity 설정
+            rstrntImageEntity.setSpotCmnt(rstrntCommentEntity);
+            rstrntImageEntity.setSpotImgUrl(imgUrl);
+
+            spotCmntImgRepository.save(rstrntImageEntity);
+        }
+
+    }
+
+    // 리뷰 리스트를 불러오는 메소드
+    public List<Object> getReviewList(int spotId, int categoryNumber) {
+        // review들을 모은 List
+        List<Object> reviewList = new ArrayList<>();
+
+        // spotId과 categoryNumber를 이용해서 spotCommentEntity에서 컬럼들을 찾는다.
+        List<SpotCommentEntity> spotCommentEntityList = spotCmntRepositoryImpl.findBySpotIdAndCategoryNumber(spotId, categoryNumber);
+
+        // 찾은 컬럼들을 하나씩 재구성해 reviewList에 넣는다.
+        for(SpotCommentEntity spotCommentEntity : spotCommentEntityList) {
+
+            HashMap<String, Object> review = new HashMap<>();   // 하나의 review를 구성하는 hashmap
+            List<String> imgUrlList = new ArrayList<>();        // review에 담기는 imageUrl의 리스트
+
+            // userId를 통해 userNickname을 찾는다.
+            MemberEntity memberEntity = memberRepository.findByUserId(spotCommentEntity.getUser().getUserId());
+            String userNickname = memberEntity.getUserNickname();
+
+            // spotCmntId를 통해 spotCmntImgRepository에서 컬럼들을 찾는다.
+            List<SpotCommentImageEntity> spotImageEntityList = spotCmntImgRepository.findBySpotCmnt(spotCommentEntity);
+
+            for(SpotCommentImageEntity spotCommentImageEntity : spotImageEntityList) {
+                imgUrlList.add(spotCommentImageEntity.getSpotImgUrl());  // imgUrlList에 추가
+            }
+
+            // 하나의 리뷰를 구성해준다.
+            review.put("userNickname", userNickname);
+            review.put("rstrntCmntContent", spotCommentEntity.getSpotCmntContent());
+            review.put("rstrntCmntTime", spotCommentEntity.getSpotCmntTime());
+            review.put("rstrntCmntStar", spotCommentEntity.getSpotCmntStar());
+            review.put("categoryNumber", spotCommentEntity.getCategoryNumber());
+            review.put("rstrntCmntImg", imgUrlList);
+
+            // 최종적으로 reviewList에 넣어준다.
+            reviewList.add(review);
+
+        }
+
+        return reviewList;
+    }
+
+
+
+
 
 }

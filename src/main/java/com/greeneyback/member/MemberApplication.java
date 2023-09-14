bdf1e8e408d2d2b798f809579401e3448f91515b
mk020 <mk02081@naver.com>
2023-09-14 22:19:23 +0900

    [add] - 하루 랜덤 3개 챌린지 보여주기

--- src/main/java/com/greeneyback/member/MemberApplication.java
+++ src/main/java/com/greeneyback/member/MemberApplication.java
@@ -2,8 +2,10 @@ package com.greeneyback.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MemberApplication {
 
 	// s3 static 변수 추가

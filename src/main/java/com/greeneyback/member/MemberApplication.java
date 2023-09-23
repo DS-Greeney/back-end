a004706fb1edb3ab7dc6c1ec2e47d0f79ac2545e
KangSumin <eeeebul@naver.com>
2023-09-23 11:05:59 +0900

    [mod] - 검색기능 수정 완료

--- src/main/java/com/greeneyback/member/MemberApplication.java
+++ src/main/java/com/greeneyback/member/MemberApplication.java
@@ -3,6 +3,7 @@ package com.greeneyback.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
